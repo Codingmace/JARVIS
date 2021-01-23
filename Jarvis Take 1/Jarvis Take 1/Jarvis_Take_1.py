df81d74f11f0ab4408a384a005887ab1da9d82e8
Codingmace <codingmace@gmail.com>
2021-01-23 13:27:39 -0800

    Base
    Base Program of someone. What I am going off of.

---
+++ Jarvis Take 1/Jarvis Take 1/Jarvis_Take_1.py
@@ -0,0 +1,260 @@
+import pyttsx3
+import wikipedia
+import speech_recognition as sr
+import webbrowser
+import datetime
+import os
+import sys
+import smtplib
+import psutil
+import pyjokes
+import pyautogui
+from news import speak_news, getNewsUrl
+from diction import translate
+from loc import weather
+from youtube import youtube
+import psutil
+import pyjokes
+from sys import platform
+import os
+import getpass
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+# print(voices[0].id)
+
+## Things to put in the settings
+name = "SIR"
+platform = "win32"
+musicPath= "D://music//"
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+# Add setting and maybe a tag
+def screenshot():
+    img = pyautogui.screenshot()
+    img.save('screenshot/screenshot.png')
+
+# Do all diagnostics here
+def cpu():
+    usage = str(psutil.cpu_percent())
+    speak("CPU is at"+usage)
+
+    battery = psutil.sensors_battery()
+    speak("battery is at")
+    speak(battery.percent)
+
+# Just because
+def joke():
+    for i in range(5):
+        speak(pyjokes.get_jokes()[i])
+
+
+
+def takeCommand():
+    
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening...')
+        r.pause_threshold = 1
+        r.energy_threshold = 494
+        r.adjust_for_ambient_noise(source, duration=1.5)
+        audio = r.listen(source)
+
+    try:
+        print('Recognizing..')
+        query = r.recognize_google(audio, language='en-in')
+        print(f'User said: {query}\n')
+
+    except Exception as e:
+        # print(e)
+
+        print('Say that again please...')
+        return 'None'
+    return query
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Good Morning", name)
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon",name)
+
+    else:
+        speak('Good Evening',name)
+
+    weather()
+    speak('I am JARVIS. Please tell me how can I help you SIR?')
+
+# Definately doesn't work
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('email', 'password')
+    server.sendmail('email', to, content)
+    server.close()
+
+
+
+if __name__ == '__main__':
+    if platform == "linux" or platform == "linux2":
+        chrome_path = '/usr/bin/google-chrome'
+
+    elif platform == "darwin":
+        chrome_path = 'open -a /Applications/Google\ Chrome.app'
+
+    elif platform == "win32":
+        chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+    else:
+        print('Unsupported OS') # Search for it
+        exit(1)
+
+    webbrowser.register(
+        'chrome', None, webbrowser.BackgroundBrowser(chrome_path))
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia....')
+            query = query.replace('wikipedia', '')
+            results = wikipedia.summary(query, sentences=2)
+            speak('According to Wikipedia')
+            print(results)
+            speak(results)
+
+        elif 'youtube downloader' in query:
+            exec(open('youtube_downloader.py').read())
+
+        elif 'voice' in query:
+            if 'female' in query:
+                engine.setProperty('voice', voices[1].id)
+            else:
+                engine.setProperty('voice', voices[0].id)
+            speak("Hello",name, ", I have switched my voice. How is it?")
+
+        if 'jarvis are you there' in query:
+            speak("Yes SIR, at your service")
+
+        elif 'open youtube' in query:
+
+            webbrowser.get('chrome').open_new_tab('https://youtube.com')
+
+        elif 'cpu' in query:
+            cpu()
+
+        elif 'joke' in query:
+            joke()
+
+        elif 'screenshot' in query:
+            speak("taking screenshot")
+            screenshot()
+
+        elif 'open google' in query:
+            webbrowser.get('chrome').open_new_tab('https://google.com')
+
+        elif 'open stackoverflow' in query:
+            webbrowser.get('chrome').open_new_tab('https://stackoverflow.com')
+
+        elif 'play music' in query:
+            os.startfile(musicPath)
+
+        elif 'search youtube' in query:
+            speak('What you want to search on Youtube?')
+            youtube(takeCommand())
+            
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f'{name}, the time is {strTime}')
+
+        elif 'search' in query:
+            speak('What do you want to search for?')
+            search = takeCommand()
+            url = 'https://google.com/search?q=' + search
+            webbrowser.get('chrome').open_new_tab(url)
+            speak('Here is What I found for' + search)
+
+        elif 'location' in query:
+            speak('What is the location?')
+            location = takeCommand()
+            url = 'https://google.nl/maps/place/' + location + '/&amp;'
+            webbrowser.get('chrome').open_new_tab(url)
+            speak('Here is the location ' + location)
+
+        elif 'your name' in query:
+            speak('My name is JARVIS')
+            
+        elif 'stands for' in query:
+            speak('J.A.R.V.I.S stands for JUST A RATHER VERY INTELLIGENT SYSTEM')
+
+       """ elif 'open code' in query:
+            if platform == "win32":
+                os.startfile(
+                    "C:\\Users\\gs935\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe")
+            elif platform == "linux" or platform == "linux2" or "darwin":
+                os.system('code .') """ # Write some code for me
+
+        elif 'shutdown' in query:
+            if platform == "win32":
+                os.system('shutdown /p /f')
+            elif platform == "linux" or platform == "linux2" or "darwin":
+                os.system('poweroff')
+
+        elif 'github' in query:
+            webbrowser.get('chrome').open_new_tab(
+                'https://github.com/gauravsingh9356')
+
+        elif 'remember that' in query: ## Add to take in the entry name
+            speak("what should i remember",name)
+            rememberMessage = takeCommand()
+            speak("Ok. I will remember",rememberMessage)
+            remember = open('data.txt', 'w')
+            remember.write(rememberMessage)
+            remember.close()
+
+        elif 'do you remember anything' in query:# print out the recently added comamnds
+            remember = open('data.txt', 'r')
+            speak("you said me to remember that" + remember.read())
+
+        elif 'sleep' in query:
+            sys.exit()
+
+        elif 'dictionary' in query:
+            speak('What you want to search in your intelligent dictionary?')
+            translate(takeCommand())
+
+
+
+        elif 'news' in query:
+            speak('Ofcourse sir..')
+            speak_news()
+            speak('Do you want to read the full news...')
+            test = takeCommand()
+            if 'yes' in test:
+                speak('Ok Sir, Opening browser...')
+                webbrowser.open(getNewsUrl())
+                speak('You can now read the full news from this website.')
+            else:
+                speak('No Problem Sir')
+
+        elif 'voice' in query:
+            if 'female' in query:
+                engine.setProperty('voice', voices[0].id)
+            else:
+                engine.setProperty('voice', voices[1].id)
+            speak("Hello Sir, I have switched my voice. How is it?")
+
+        elif 'email to gaurav' in query:
+            try:
+                speak('What should I say?')
+                content = takeCommand()
+                to = 'email'
+                sendEmail(to, content)
+                speak('Email has been sent!')
+
+            except Exception as e:
+                speak('Sorry sir, Not able to send email at the moment')

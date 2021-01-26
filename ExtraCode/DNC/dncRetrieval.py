62c893bde579ed422084cd8d58ea4fb6f72536b7
Codingmace <codingmace@gmail.com>
2021-01-26 11:24:00 -0600

    Version 0.25
    Updated the DNC for subject Compression Method Mapping

--- ExtraCode/DNC/dncRetrieval.py
+++ ExtraCode/DNC/dncRetrieval.py
@@ -18,12 +18,25 @@ for line in a:
     states.append(split[0])
     abbr.append(split[1][:2])
 
+# Map Subject
+subject = ['']
+corr = [''] # Correlation
+with open('subjectMap.txt','r') as subject_file:
+    a = subject_file.readlines()
+for line in a:
+    split = line.split(" - ")
+    subject.append(split[0])
+    corr.append(split[1])
+#print(subject)
+
 # Read in from the API
 # for offset in range(0 ,3186831, 50):
 for offset in range(0 ,300, 50):
     response = requests.get(baseUrl+apiKey + "&" + extendUrl + str(offset))
     data = response.json()
     output = open(str("Data\output" + str((int)(offset / 50))+".json"), "w")
+
+    print(data)
 # Parse Json
     del data['meta']
     del data['links']
@@ -36,7 +49,21 @@ for offset in range(0 ,300, 50):
 
         element['number'] = element['attributes']['company-phone-number']
         element['date'] = element['attributes']['violation-date']
-        element['subject'] = element['attributes']['subject'].replace("  ", " ")
+        tempSubject =  element['attributes']['subject'].replace("  ", " ")
+       # element['subject'] = element['attributes']['subject'].replace("  ", " ")
+
+#        print(tempSubject)
+# Modify so that we get the attributes Compressed
+        if tempSubject in subject:
+#            print(subject.index(tempSubject))
+            tempSubject = corr[subject.index(tempSubject)]
+#            print(tempSubject)
+        else:
+            print(tempSubject)
+
+        
+        element['subject'] = tempSubject
+        
         element['recorded-or-robo']= element['attributes']['recorded-message-or-robocall']
         element['city'] = element['attributes']['consumer-city']
         curState = element['attributes']['consumer-state']

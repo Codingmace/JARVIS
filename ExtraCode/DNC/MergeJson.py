1fad4c5a1ae3fbb7894a9e19439c600be425a57c
Codingmace <codingmace@gmail.com>
2021-01-26 14:20:48 -0600

    Version 0.28
    Adding in some security

--- ExtraCode/DNC/MergeJson.py
+++ ExtraCode/DNC/MergeJson.py
@@ -1,15 +1,29 @@
 import os
 import json
+from jsonmerge import merge  #NEED TO DOWNLOAD THIS 
 
 files = os.listdir("Data")
 print(files)
 f = open('master.json','a')
+dictA = dict()
+out = dict()
 for file in files:
     if ".json" in file:
         f2 = open(('Data\\' + file), 'r')
-        print(file)
-        data = json.load(f2)
-        json.dump(data,f)
+        dictB = json.load(f2)
+#        dictA.update(dictB)
+        out = merge(dictA, dictB)
+        dictA = out
+#        print(dictB + dictB.update(dictA))
+#        print(len(dictB['data']))
+        #out = dictA.items() + dictB.items()
+       # dictA = out
+#        merged_dict = {key: value for (key, value) in (dictA.items() + dictB.items())}
+#        dictA = merged_dict
+#        jsonString_merged = json.dumps(merged_dict)
+        print(len(json.dumps(dictA)))
+
+json.dump(dictA,f)
 
 f.flush()
 f.close()

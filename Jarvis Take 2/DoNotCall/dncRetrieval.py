5850dc909f3ee12ad6e46f5e1c98aef56cf9da15
Codingmace <codingmace@gmail.com>
2021-01-27 14:14:26 -0600

    Version 1.08
    Copy and Cleanup

---
+++ Jarvis Take 2/DoNotCall/dncRetrieval.py
@@ -0,0 +1,111 @@
+import requests
+import json
+import time
+# https://www.ftc.gov/developer
+
+# testUrl = 'https://api.ftc.gov/v0/dnc-complaints?api_key=DEMO_KEY&created_date_from="2020-02-27 04:10:00"&created_date_to="2020-02-27 04:30:00"'
+baseUrl = "https://api.ftc.gov/v0/dnc-complaints?api_key="
+extendUrl = "offset="
+apiDoc = open("APIKey.txt", "r")
+apiKey = apiDoc.readline()
+apiKey = apiDoc.readline()
+
+### FOR UPDATING
+
+# NEED TO REMOVE BECAUSE YOU CANNOT HAVE MORE THAN 1 VALID KEY AT A TIME
+
+# Map States
+states = ['']
+abbr = ['']
+with open('stateMap.txt','r') as map_file:
+    a = map_file.readlines()
+    
+for line in a:
+    split = line.split(" - ")
+    states.append(split[0])
+    abbr.append(split[1][:2])
+
+# Map Subject
+subject = ['']
+corr = [''] # Correlation
+with open('subjectMap.txt','r') as subject_file:
+    a = subject_file.readlines()
+    
+for line in a:
+    split = line.split(" - ")
+    subject.append(split[0])
+    corr.append(split[1].strip('\n'))
+            
+phoneMap = open("PhoneMap.txt", "w")
+errors = open("log.txt","a") # Adjust to be correct folder and not override eachother
+
+# Read in from the API
+# for offset in range(7500,3186831, 50):
+# for offset in range(9350,3186831, 50):
+# for offset in range(9550,3186831, 50):
+for offset in range(11350,3186831, 50):
+    time.sleep(60)
+    response = requests.get(baseUrl+apiKey + "&" + extendUrl + str(offset))
+    
+    if not (response.status_code == 200):
+        print("We are out of responses. Ended on offset of " + str(offset))
+        errors.write("Out of request, "+ str(offset) + "\n")
+        errors.flush()
+        errors.close()
+        phoneMap.flush()
+        phoneMap.close()
+        exit()
+            
+    data = response.json()
+    print(response.status_code)
+    output = open(str("Data\output" + str((int)(offset / 50))+".json"), "w")
+    print(data)
+    
+# Parse Json
+    del data['meta']
+    del data['links']
+    
+    for element in data['data']:
+        del element['type']
+        del element['relationships']
+        del element['meta']
+        del element['links']
+
+        element['number'] = element['attributes']['company-phone-number']
+        element['date'] = element['attributes']['violation-date']
+        tempSubject =  element['attributes']['subject'].replace("  ", " ")
+       # element['subject'] = element['attributes']['subject'].replace("  ", " ")
+
+
+# Modify so that we get the attributes Compressed
+        if tempSubject in subject:
+            tempSubject = corr[subject.index(tempSubject)]
+        else: # If it is not mapped yet
+            print(tempSubject)
+
+        
+        element['subject'] = tempSubject
+        
+        element['recorded-or-robo']= element['attributes']['recorded-message-or-robocall']
+        element['city'] = element['attributes']['consumer-city']
+        curState = element['attributes']['consumer-state']
+        curAbbr = abbr[states.index(curState)]
+        
+        element['state'] = curAbbr
+        element['area-code'] = element['attributes']['consumer-area-code']
+        
+        del element['attributes']
+        temp = element['id'] + " " + element['number'] + "\n"
+        phoneMap.write(temp)
+
+        
+    # Write it all to a file
+    json.dump(data, output)
+    phoneMap.flush()
+    errors.flush()
+    output.flush()
+    output.close()
+    
+errors.close()
+phoneMap.close()
+
---
+++ Jarvis Take 2/apiHandler.py
@@ -0,0 +1,75 @@
+## This is to help with importing the API only when needed
+## Will read in all the keys here though
+
+# Urban Dictionary : To define words
+from urbanDictionary import defineWord
+
+# Google Search : Look up things on google
+from googleSearch import search, images, crawl, news
+
+def encode(term):
+    return term.replace("+", "%2B").replace(" ", "+")
+
+# Revese Image : Google Reverse Image Search
+from reverseImage import reverseImage
+
+
+# Open Weather : Reports back the weather
+""" NEEDS: have to sort out how I am going to do location """
+from openWeather import *
+
+# Weather.com : Reports back weather
+""" Just Need Geocode """
+from weatherCom import *
+
+# Open Proxy : Reports all open proxies at that moment
+from openProxies import openProxy
+
+# Cat Facts : Returns random cat facts
+from catFacts import catFact
+
+# Test : To grab text, identify parts of speech, identify name of entities
+from testTextHelp import fetchText, pos, namedEntity
+
+# Email Validate : Check if email is vali or not
+from emailValidate import validEmail
+
+# Summarize API : Summarize the text of a URL link
+from summarizeApi import summarize
+
+# Verifone : Verifies a phone is valid (Default Country is US)
+from veriphone import verifyPhone
+
+# Pose Estimate : Takes a photo or video and estimates the posture
+from poseEstimate import video, image
+
+# Transcribe : Turns audio into text
+from trascribe import transcribeUrl, getTask, serviceStatus, getTasks, transcribe
+
+# Trulia : 
+from trulia import searching, propertyDetail
+
+def encode(term): # URL Encoding
+    return term.replace(" ", "%20")
+
+# COST MONEY
+# Category Prediction : Prints out catagories the text matches
+from categoryPrediction import categoryPrediction
+
+# Text Analyzer : Analyzes the contents of a URL
+from textAnalyzer import contentExtract, namedExtract, partOfSpeech
+
+# URL Intel :
+from urlIntel import urlIntel
+
+# Plate Recognition :
+from plateRecognition import recognizeByUrl , recognizeByImage
+### USES TRUE URL ENCODING
+
+# OCRLY : URL Image to text
+from OCRLY import OImage2Text
+
+# Youtube Download : Download Youtube Video by VideoID
+from youtubeDownload import downloadVideo
+
+
